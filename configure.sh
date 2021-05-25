9e4084ea8fe2b7c18a857fceeae72158f2a2caa2
Stevan Dupor <smdupor@gmail.com>
2021-05-25 13:40:13 -0400

    Modify setup

--- configure.sh
+++ configure.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+
+#sets up iptables firewall on NCSU VCL
+
 sudo iptables -A INPUT -p tcp --dport 65432 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --dport 65432 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
